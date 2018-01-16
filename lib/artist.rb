1270caee23dee89b4590cd1f9b8b6301c10cc10c
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-16 15:40:25 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,15 +13,15 @@ class Artist
 
   def add_song(song)
     @songs << song
-    song.artist = self
     @@song_count += 1
+    song.artist = self
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
-    song.artist = self
     @@song_count += 1
+    song.artist = self
   end
 
   def self.song_count
